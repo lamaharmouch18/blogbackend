70a6a44c4173a1451d8a370e6e63292c5d1f454a
lamaharmouch18 <52330808@students.liu.edu.lb>
2026-01-14 16:12:19 +0200

    Set port to 8080 for Railway

--- index.js
+++ index.js
@@ -29,9 +29,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-const PORT = process.env.PORT || 8800;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
